f1446f4f5f39d6aef1cd43ddf7592ba89a197108
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2012-10-10 16:22:29 -0400

    Template parametrization for selecting the modelling algorithm.

--- include/rlfd_segmentation/DHMMSegmenter.hh
+++ include/rlfd_segmentation/DHMMSegmenter.hh
@@ -1,5 +1,25 @@
+/**
+ * Skills segmentation and learning for Robot Learning by Demonstration
+ * Copyright (C) 2012  Pierre-Luc Bacon <pierre-luc.bacon@mail.mcgill.ca> 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
 #ifndef __DHMMSEGMENTER_H__
 #define __DHMMSEGMENTER_H__
+
 #include <numeric>
 #include <Eigen/Core>
 #include <rlfd_segmentation/Model.hh>
@@ -9,6 +29,7 @@
 namespace rlfd {
 /**
  * A class to perform time series segmentation.
+ * It takes as a argument a Model type
  *
  * Implements a variant of:
  *
@@ -17,25 +38,12 @@ namespace rlfd {
  * 14 (NIPS 2001) (pp. 793–800). Vancouver, British
  * Columbia, Canada: MIT Press.
  */
+template <class WindowModel>
 class DHMMSegmenter
 {
 public:
-    DHMMSegmenter(double varsigma=0, double k=0);
+    DHMMSegmenter(unsigned W=50, double varsigma=0, double k=0);
     virtual ~DHMMSegmenter() {};
-
-    /**
-     * Add an observation (delay-embedded). 
-     * When the number of observations is sufficient
-     * to build a model, the segmentation will get 
-     * updated automatically
-     * @param x An observation vector 
-     */
-    void AddObservation(const Eigen::VectorXd& x);
-
-    /**
-     * Regularization constant C that subsumes varsigma and k
-     */
-    double GetRegularizer() { return 2*std::pow(varsigma, 2)*std::log(k); }
 
 private:
     struct State {
@@ -52,7 +60,6 @@ private:
         // The prototype assigned to this state
         std::shared_ptr<Model> prototype;
     };
-
     std::deque<State> states;
 
     // Maintain a history of the optimal paths costs
@@ -60,6 +67,47 @@ private:
 
     double varsigma;
     double k;
+
+public:
+    /**
+     * Regularization constant C that subsumes varsigma and k
+     */
+    constexpr double GetRegularizer() { return 2*std::pow(varsigma, 2)*std::log(k); }
+
+    /**
+     * Add an observation (delay-embedded). 
+     * When the number of observations is sufficient
+     * to build a model, the segmentation will get 
+     * updated automatically
+     * @param x An observation vector 
+     */
+    void AddObservation(const Eigen::VectorXd& x)
+    {
+        // Compute a new model 
+        Model* latest_model = new WindowModel(); 
+        states.push_front(State(latest_model->distance(*(states[0].model)), latest_model, latest_model));
+
+        // t=0 to t-1
+        for (unsigned t = 1; t < states.size()-1; t++) {
+            // Cost for a constrained path finishing with the new model r
+            // starting at 0 and finishing at time step t 
+            states.front().cpath = latest_model->distance(*(states[t].model)) + std::min(states.front().cpath, opaths[t-1] + GetRegularizer()); 
+            if (states.front().cpath < opaths[t]) {
+                opaths[t] = states.front().cpath;
+                states[t].prototype = std::shared_ptr<Model>(latest_model); // Not sure if that makes sense
+            } 
+        }
+
+        // Compute the minimum cost for each state
+        for (unsigned s = 0; s < states.size(); s++) {
+            states[s].cpath = latest_model->distance(*(states[s].model)) + std::min(states[s].cpath, opaths.front() + GetRegularizer());
+        }
+
+        // Find the mininum cost constrained path
+        auto mincost_state = std::min_element(states.begin(), states.end(), [](const State& a, const State& b) { return a.cpath < b.cpath; });
+        opaths.push_front(mincost_state->cpath);
+        states.front().prototype = mincost_state->model; 
+    }
 };
 
 } // namespace rlfd
