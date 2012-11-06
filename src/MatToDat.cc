bbc6798ca488616a5bea0dfc7307aa91d7e6fb7c
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2012-11-06 01:21:13 -0500

    Add a utility for converting Matlab's .mat to .dat.
    Writes to STDOUT:
    mattodat out.mat | gnuplot -p -e 'plot "-" with linespoints'

---
+++ src/MatToDat.cc
@@ -0,0 +1,41 @@
+/**
+ * Skills segmentation and learning for Robot Learning by Demonstration
+ * Copyright (C) 2012  Pierre-Luc Bacon <pierre-luc.bacon@mail.mcgill.ca> 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+#include <rlfd/utils/Matio.hh>
+#include <Eigen/Core>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  if (argc != 2) {
+    std::cerr << "Convert a Matlab's .mat file to a tabular raw .dat file" << std::endl;
+    std::cerr << "Usage: mattodat [FILE]" << std::endl;
+    return -1;
+  }
+  // Import 
+  rlfd::utils::Matio mat;
+  mat.Open(argv[1]);
+  Eigen::MatrixXd out;
+  mat.Read(out);
+  mat.Close();
+
+  std::cout << out << std::endl;
+
+  return 0;
+}
