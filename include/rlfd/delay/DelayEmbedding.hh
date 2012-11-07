4af13229a60c281e2d55a40b35f6318059c1af53
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2012-11-07 13:19:26 -0500

    Implement the delay embedding function
    Given a time series, a M x m matrix where M = N- (m-1)*lag is returned.
    A standalone command line utility is also provided.

--- include/rlfd/delay/DelayEmbedding.hh
+++ include/rlfd/delay/DelayEmbedding.hh
@@ -1,6 +1,6 @@
 /**
  * Skills segmentation and learning for Robot Learning by Demonstration
- * Copyright (C) 2012  Pierre-Luc Bacon <pierre-luc.bacon@mail.mcgill.ca> 
+ * Copyright (C) 2012  Pierre-Luc Bacon <pierre-luc.bacon@mail.mcgill.ca>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -17,10 +17,11 @@
  * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301, USA.
  */
-#ifndef __DELAYEMBEDDING_HH__
-#define __DELAYEMBEDDING_HH__
+#ifndef __DELAY_EMBEDDING_HH__
+#define __DELAY_EMBEDDING_HH__
 
 #include <rlfd/Model.hh>
+#include <Eigen/Core>
 
 namespace rlfd {
 namespace delay {
@@ -28,8 +29,27 @@ namespace delay {
 class DelayEmbedding : Model {
  public:
   double distance(const Model& other) const override { return 0; };
+
+  /**
+   * Embbed a sclar time series
+   * @param ts The measured time series
+   * @param m The embedding dimension
+   * @param lag The lag parameter
+   * @param out M x m matrix where M = N-(m-1)*lag
+   */
+  static void Embed(const Eigen::VectorXd& ts, int m, int lag, Eigen::MatrixXd& out)
+  {
+    int M = ts.size() - (m-1)*lag;
+    out.resize(M, m);
+
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j <= (m-1); j++) {
+        out(i, j) = ts[i + j*lag];
+      }
+    }
+  }
 };
 
 } // namespace delay
 } // namespace rlfd
-#endif
+#endif // __DELAY_EMBEDDING_HH__
